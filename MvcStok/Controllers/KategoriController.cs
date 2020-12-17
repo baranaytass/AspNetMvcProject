e6b6ab22242b06c49520b61b854e9c658a54d03b
Baran Aytaş <baranaytass@github.com>
2020-12-17 21:10:46 +0300

    MvcProject

---
+++ MvcStok/Controllers/KategoriController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcStok.Models.Entity;
+
+namespace MvcStok.Controllers
+{
+    public class KategoriController : Controller
+    {
+        // GET: Kategori
+        DbMvcStokEntities db = new DbMvcStokEntities();
+        public ActionResult Index()
+        {
+            var degerler = db.TBLKATEGORILER.ToList();
+            return View(degerler);
+        }
+        [HttpGet]
+        public ActionResult YeniKategori()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult YeniKategori(TBLKATEGORILER p1)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("YeniKategori");
+            }
+            db.TBLKATEGORILER.Add(p1);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult Sil(int id)
+        {
+            var kategori = db.TBLKATEGORILER.Find(id);
+            db.TBLKATEGORILER.Remove(kategori);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult KategoriGetir(int id)
+        {
+            var ktg = db.TBLKATEGORILER.Find(id);
+            return View("KategoriGetir", ktg);
+        }
+        public ActionResult Guncelle(TBLKATEGORILER p1)
+        {
+            var kategori = db.TBLKATEGORILER.Find(p1.KATEGORIID);
+            kategori.KATEGORIAD = p1.KATEGORIAD;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
